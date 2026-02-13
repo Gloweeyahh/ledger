f8607d4cf974d6dd7fa6714af0e43b5bfce5a4d4
Gloweeyahh <glowemeka@gmail.com>
2026-02-13 15:02:06 +0100

    final edit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,93 +25,98 @@ const Sidebar = ({ logoSrc }) => {
   };
 
   return (
-    <aside className={`${styles.aside} ${menuOpen ? styles.open : ""}`}>
-      <div className={styles.hamburger} onClick={toggleMenu}>
-        {menuOpen ? (
-          <FaChevronLeft className={styles.closeIcon} />
-        ) : (
-          <>
-            <div className={`${styles.hamburgerLine}`} />
-            <div className={`${styles.hamburgerLine}`} />
-            <div className={`${styles.hamburgerLine}`} />
-          </>
-        )}
-      </div>
-      <div className={styles.mainLogo}>
-        <Logo />
-      </div>
-      <div className={styles.sidebar_items}>
+    <>
+      <aside className={`${styles.aside} ${menuOpen ? styles.open : ""}`}>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          {menuOpen ? (
+            <FaChevronLeft className={styles.closeIcon} />
+          ) : (
+            <>
+              <div className={`${styles.hamburgerLine}`} />
+              <div className={`${styles.hamburgerLine}`} />
+              <div className={`${styles.hamburgerLine}`} />
+            </>
+          )}
+        </div>
+        <div className={styles.mainLogo}>
+          <Logo />
+        </div>
+        <div className={styles.sidebar_items}>
           <Logo className={styles.mobileLogo} />
-        <ul aria-label="Sidebar Lists" className={styles.mainList}>
-          <li>
-            <FaChartPie />
-            <p>Portfolio</p>
-          </li>
-          <li>
-            <FaChartLine />
-            <p>Markets</p>
-          </li>
-          <li>
-            <FaWallet />
-            <p>Accounts</p>
-          </li>
-          <li>
-            <PiArrowLineUpBold />
-            <p>Send</p>
-          </li>
-          <li>
-            <PiArrowLineDownBold />
-            <p>Receive</p>
-          </li>
-          <li>
-            <PiCurrencyDollarSimpleBold />
-            <p>Buy/Sell</p>
-          </li>
-          <li>
-            <PiArrowsLeftRightBold />
-            <p>Swap</p>
-          </li>
-          <li>
-            <FaUserGear />
-            <p>Manager</p>
-          </li>
-        </ul>
-        <ul aria-label="Sidebar Lists" className={styles.mobileList}>
-          <li>
-            <FaChartPie />
-            <p>Portfolio</p>
-          </li>
-          <li>
-            <FaChartLine />
-            <p>Markets</p>
-          </li>
-          <li>
-            <FaWallet />
-            <p>Accounts</p>
-          </li>
-          <li>
-            <PiArrowLineUpBold />
-            <p>Send</p>
-          </li>
-          <li>
-            <PiArrowLineDownBold />
-            <p>Receive</p>
-          </li>
-          <li>
-            <PiCurrencyDollarSimpleBold />
-            <p>Buy/Sell</p>
-          </li>
-          <li>
-            <PiArrowsLeftRightBold />
-            <p>Swap</p>
-          </li>
-          <li>
-            <FaUserGear />
-            <p>Manager</p>
-          </li>
-        </ul>
-      </div>
-    </aside>
+          <ul aria-label="Sidebar Lists" className={styles.mainList}>
+            <li>
+              <FaChartPie />
+              <p>Portfolio</p>
+            </li>
+            <li>
+              <FaChartLine />
+              <p>Markets</p>
+            </li>
+            <li>
+              <FaWallet />
+              <p>Accounts</p>
+            </li>
+            <li>
+              <PiArrowLineUpBold />
+              <p>Send</p>
+            </li>
+            <li>
+              <PiArrowLineDownBold />
+              <p>Receive</p>
+            </li>
+            <li>
+              <PiCurrencyDollarSimpleBold />
+              <p>Buy/Sell</p>
+            </li>
+            <li>
+              <PiArrowsLeftRightBold />
+              <p>Swap</p>
+            </li>
+            <li>
+              <FaUserGear />
+              <p>Manager</p>
+            </li>
+          </ul>
+          <ul aria-label="Sidebar Lists" className={styles.mobileList}>
+            <li>
+              <FaChartPie />
+              <p>Portfolio</p>
+            </li>
+            <li>
+              <FaChartLine />
+              <p>Markets</p>
+            </li>
+            <li>
+              <FaWallet />
+              <p>Accounts</p>
+            </li>
+            <li>
+              <PiArrowLineUpBold />
+              <p>Send</p>
+            </li>
+            <li>
+              <PiArrowLineDownBold />
+              <p>Receive</p>
+            </li>
+            <li>
+              <PiCurrencyDollarSimpleBold />
+              <p>Buy/Sell</p>
+            </li>
+            <li>
+              <PiArrowsLeftRightBold />
+              <p>Swap</p>
+            </li>
+            <li>
+              <FaUserGear />
+              <p>Manager</p>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
+      {/* Overlay */}
+      {menuOpen && <div className={styles.overlay} onClick={toggleMenu} />}
+    </>
   );
 };
 
